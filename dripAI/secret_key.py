cd2910a83f6c9a1e8569d6eaaaaa629d7a10ad10
bala-guhan <129078348+bala-guhan@users.noreply.github.com>
2024-03-14 23:14:02 +0530

    Add files via upload

---
+++ dripAI/secret_key.py
@@ -0,0 +1,7 @@
+openai_api_key = "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"
+serp_api_key = "27f4173f06ff5f9783766410243d1c4f12122e5f338851ed3b6b57f6d4991a6f"
+scenex_api_key = "15NxqduLzSGJMv5R8eyY:0b2f7bba32cf354cf55daec80fed7c3ff7d611f31ae51ab0598cdea96e7d73a3"
+eleven_api_key = "6f6a4524976bb03c51bbf47568692b2b"
+apify_api_key = "apify_api_WcCf02MKtx4cfVvZMrUgSIzBf3puvC2tNt14"
+
+
